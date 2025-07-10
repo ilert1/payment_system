e3d33422da7d6ddf3b190aeb34fd6d260d3582e9
David <email@mail.com>
2025-07-10 14:16:59 +0400

    fix: Small fixes

--- source/src/hooks/usePaymentPage.tsx
+++ source/src/hooks/usePaymentPage.tsx
@@ -1,8 +1,8 @@
-import { PAGE_MAIN } from "../shared/assets/constants.js";
 import { useEffect } from "react";
 import { useAppContext } from "../AppContext.js";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useBFStore } from "@/shared/store/bfDataStore.js";
+import { AppRoutes } from "@/shared/const/router.js";
 
 export default function usePaymentPage({ absolutePath = false }) {
     const { paymentEcomPage } = useAppContext();
@@ -22,7 +22,7 @@ export default function usePaymentPage({ absolutePath = false }) {
                 locationSplits.length > 1 &&
                 locationSplits.slice(0, -1).join("/") + "/" + paymentPage !== location.pathname
             ) {
-                if (!location.pathname.includes(paymentPage) && paymentPage !== PAGE_MAIN) {
+                if (!location.pathname.includes(paymentPage) && paymentPage !== AppRoutes.PAGE_MAIN) {
                     console.log(
                         "Navigating to: ",
                         absolutePath || paymentPage.includes("/") ? paymentPage : "../" + paymentPage
@@ -30,7 +30,7 @@ export default function usePaymentPage({ absolutePath = false }) {
                     nav(absolutePath || paymentPage.includes("/") ? paymentPage : "../" + paymentPage, {
                         replace: true
                     });
-                } else if (paymentPage === PAGE_MAIN && locationSplits.length > 3 && locationSplits[3]) {
+                } else if (paymentPage === AppRoutes.PAGE_MAIN && locationSplits.length > 3 && locationSplits[3]) {
                     console.log("Navigating to: ", "..");
                     nav("..", { replace: true });
                 }
@@ -38,3 +38,35 @@ export default function usePaymentPage({ absolutePath = false }) {
         }
     }, [status]);
 }
+
+// import { PAGE_MAIN } from "../assets/constants.js";
+// import { useContext, useEffect } from "react";
+// import AppContext from "../AppContext";
+// import { useLocation } from "react-router-dom";
+
+// export default function usePaymentPage({ absolutePath = false }) {
+//     const { paymentEcomPage, navigate, status } = useContext(AppContext);
+
+//     const location = useLocation();
+//     const nav = navigate();
+
+//     useEffect(() => {
+//         const paymentPage = paymentEcomPage();
+//         const locationSplits = location.pathname.split("/");
+
+//         if (paymentPage) {
+//             if (
+//                 locationSplits.length > 1 &&
+//                 locationSplits.slice(0, -1).join("/") + "/" + paymentPage !== location.pathname
+//             ) {
+//                 if (!location.pathname.includes(paymentPage) && paymentPage !== PAGE_MAIN) {
+//                     nav(absolutePath || paymentPage.includes("/") ? paymentPage : "../" + paymentPage, {
+//                         replace: true
+//                     });
+//                 } else if (paymentPage === PAGE_MAIN && locationSplits.length > 3 && locationSplits[3]) {
+//                     nav("..", { replace: true });
+//                 }
+//             }
+//         }
+//     }, [status]);
+// }
