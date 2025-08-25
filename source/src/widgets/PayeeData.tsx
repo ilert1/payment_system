88b165f0af25d96da3d19042c8a5ef67906da0ae
David <email@mail.com>
2025-08-25 16:03:35 +0400

    fix: Fixed import

--- source/src/widgets/PayeeData.tsx
+++ source/src/widgets/PayeeData.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable react/prop-types */
+import { useTranslation } from "react-i18next";
+import DefaultBankIcon from "@/shared/assets/images/bank.svg";
+import { formatedRequisite } from "@/shared/lib/formattedRequisite";
+import { useBFStore } from "@/shared/store/bfDataStore";
 import { useAppContext } from "../AppContext";
 import CardsIcon from "../shared/assets/images/cards.svg?react";
 import DollarCircleIcon from "../shared/assets/images/dollar-circle.svg?react";
 import User2Icon from "../shared/assets/images/user2.svg?react";
-import PayeeDataItem from "./PayeeDataItem";
-
-import DefaultBankIcon from "@/shared/assets/images/bank.svg";
-import { useTranslation } from "react-i18next";
-import { useBFStore } from "@/shared/store/bfDataStore";
-import { formatedRequisite } from "@/shared/lib/formattedRequisite";
+import { PayeeDataItem } from "./PayeeDataItem";
 
 const bankIcon = (bank: string) => {
     return bank ? `/banks/${bank}.svg` : DefaultBankIcon;
