ec54daa42820f99a31e66df56ad9531bdd42f1ae
Максим Сулин <maxsaxmax@mail.ru>
2025-08-28 19:17:10 +0300

    instructions spans added, little cleanup

--- source/src/pages/PayPage/ui/PaymentInstructions.tsx
+++ source/src/pages/PayPage/ui/PaymentInstructions.tsx
@@ -55,7 +55,6 @@ export const PaymentInstructions: React.FC<PaymentInstructionsProps> = ({
                         code: caseName === tjs ? "+992" : "+994",
                         ...ns
                     })}
-                    start={0}
                 />
             </div>
         );
@@ -75,13 +74,15 @@ export const PaymentInstructions: React.FC<PaymentInstructionsProps> = ({
 
                 <InstructionItems
                     data={t("steps_with_check.steps", {
-                        requisite: t(`steps_with_check.${trader?.phone_number ? "phoneNumber" : "cardNumber"}`, ns),
+                        requisite: t(
+                            `steps_with_check.${trader?.phone || trader?.phone_number ? "phoneNumber" : "cardNumber"}`,
+                            ns
+                        ),
                         bankName: bankName,
                         amount: `${BFData?.[dest]?.amount} ${getCurrencySymbol(BFData?.[dest]?.currency ?? "")}`,
                         buttonName: t("approveTransfer", ns),
                         ...ns
                     })}
-                    start={0}
                 />
             </div>
         );
@@ -96,7 +97,7 @@ export const PaymentInstructions: React.FC<PaymentInstructionsProps> = ({
                     <p>{t("steps_transgran_tcard2card.tbankTitle", ns)}</p>
                 </div>
 
-                <InstructionItems data={t("steps_transgran_tcard2card.tbank", ns)} start={0} />
+                <InstructionItems data={t("steps_transgran_tcard2card.tbank", ns)} />
             </div>
         );
     }
@@ -111,7 +112,6 @@ export const PaymentInstructions: React.FC<PaymentInstructionsProps> = ({
                         amount: `${BFData?.[dest]?.amount}\u00A0${getCurrencySymbol(BFData?.[dest]?.currency ?? "")}`,
                         ...ns
                     })}
-                    start={0}
                 />
             </div>
         );
@@ -130,13 +130,12 @@ export const PaymentInstructions: React.FC<PaymentInstructionsProps> = ({
     return (
         <div className="instructions_new">
             <InstructionItems
-                data={t(`default_steps.${trader?.phone || trader?.phone_number ? "steps_card" : "steps_phone"}`, {
+                data={t(`default_steps.${trader?.phone || trader?.phone_number ? "steps_phone" : "steps_card"}`, {
                     amount: `${BFData?.[dest]?.amount}\u00A0${getCurrencySymbol(BFData?.[dest]?.currency ?? "")}`,
                     bankName: bankName,
                     buttonName: t("approveTransfer", ns),
                     ...ns
                 })}
-                start={0}
             />
         </div>
     );
